2633533669e230a592822ccac61ea4814abc35b2
Didac Balboa <didacbalboa2001@gmail.com>
2022-09-15 21:05:16 +0200

    RecibirDatos

--- recibir.php
+++ recibir.php
@@ -6,4 +6,22 @@ echo "<h1>".$_POST['apellidos']."</h1>";
 
 var_dump($_POST);
 
+
+/* 
+Ejercicio 4. Recoger dos numeros por la url(Parametros GET) y hacer todas las 
+ * operaciones basicas de una calculadora(suma, resta, multiplicaion y division)
+ * de esos dos numeros.
+ */
+
+
+/* 
+ Ejercicio 5. Hacer un programa que muestre todos los numeros entre dos numeros
+ * que nos lleguen por la URL($_GET)
+ */
+
+
+/* 
+ Ejercicio 6. Mostrar una tabla de HTML con las tablas de multiplicar del 1 al 10.
+ */
+
 ?>
