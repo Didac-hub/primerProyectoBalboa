74d5aa65c8105fed4904b456bb0280e4cf04fa57
Didac Balboa <58149378+Didac-hub@users.noreply.github.com>
2022-12-05 16:49:11 +0100

    ERROR de commit

--- objetos/agregarProduc.php
+++ objetos/agregarProduc.php
@@ -10,7 +10,7 @@ require_once 'objetos/productos.php';
 $entrantes = array(new Entrantes(0, 'entrante', 'Nachos',10.95,'Nachos con carne y queso','e0'),
                         new Entrantes(1, 'entrante', 'Aritos de Cebolla',8.95,'Aritos de cebolla to bonitos','e1'),
                             new Entrantes(2, 'entrante', 'Tequeños',9.95,'Tequeñitos bonitos','e2'),
-                                new Entrantes(3, 'entrante', 'Alitas de pollo',9.95,'alitas de polla','e3'));
+                                new Entrantes(3, 'entrante', 'Alitas de pollo',9.95,'alitas de poll','e3'));
 
 $hamburguesas = array(new Hamburguesa(4, 'hamburguesa', 'La Magnifica',13.50,'Hamburguesa mas magnifica','h0'),
                         new Hamburguesa(5, 'hamburguesa', 'Cheessy-Chiken',12.95,'Hambueguresa queso pollo','h1'),
@@ -18,7 +18,7 @@ $hamburguesas = array(new Hamburguesa(4, 'hamburguesa', 'La Magnifica',13.50,'Ha
                                 new Hamburguesa(7, 'hamburguesa', 'La Popular',8.95,'Hmaburguesa mas popular','h3'));
                                     
 $postres = array(new Postres(8, 'postre', 'Browniee',9.95,'Postre mitico de EEUU','p0'),
-                    new Postres(9, 'postre', 'Cookielleta',8.95,'Postre de galleta pa los gays','p1'));
+                    new Postres(9, 'postre', 'Cookielleta',8.95,'Postre de galleta','p1'));
 
 
 
